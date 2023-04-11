be987d136aa591cfe2fb2cfe4fa140c1a0c3a7dc
zlobbb1n <119886085+zlobbb1n@users.noreply.github.com>
2023-04-11 21:16:22 +0200

    About/Contact

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,49 +1,59 @@
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
+import TelegramIcon from "@mui/icons-material/Telegram";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 const Footer = () => {
-  return (
-    <>
-      <Box
-        sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
-      >
-        <Box
-          sx={{
-            my: 3,
-            "& svg": {
-              fontSize: "60px",
-              cursor: "pointer",
-              mr: 2,
-            },
-            "& svg:hover": {
-              color: "#0aa5a7",
-              transform: "translateX(5px)",
-              transition: "all 400ms",
-            },
-          }}
-        >
-          {/* icons */}
-          <InstagramIcon />
-          <TwitterIcon />
-          <GitHubIcon />
-          <YouTubeIcon />
-        </Box>
-        <Typography
-          variant="h5"
-          sx={{
-            "@media (max-width:600px)": {
-              fontSize: "1rem",
-            },
-          }}
-        >
-          Все права защищены &copy; Sunrise
-        </Typography>
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Box
+                sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
+            >
+                <Box
+                    sx={{
+                        my: 3,
+                        "& svg": {
+                            fontSize: "60px",
+                            cursor: "pointer",
+                            mr: 2,
+                            color: "white"
+                        },
+                        "& svg:hover": {
+                            color: "#0aa5a7",
+                            transform: "translateX(10px)",
+                            transition: "all 400ms",
+                        },
+
+                    }}
+                >
+                    {/* icons */}
+                    <IconButton href="https://www.instagram.com/sunrise_warsaw/" target='_blank'>
+                        <InstagramIcon />
+                    </IconButton>
+                    <IconButton href="https://t.me/sunrise_warsaw" target='_blank'>
+                        <TelegramIcon />
+                    </IconButton>
+                    <IconButton href="https://www.instagram.com/sunrise_warsaw/" target='_blank'>
+                        <GitHubIcon />
+                    </IconButton>
+                    <IconButton href="https://www.instagram.com/sunrise_warsaw/" target='_blank'>
+                        <YouTubeIcon />
+                    </IconButton>
+                </Box>
+                <Typography
+                    variant="h5"
+                    sx={{
+                        "@media (max-width:600px)": {
+                            fontSize: "1rem",
+                        },
+                    }}
+                >
+                    Все права защищены &copy; Sunrise
+                </Typography>
+            </Box>
+        </>
+    );
 };
 
 export default Footer;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,73 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import MailIcon from "@mui/icons-material/Mail";
+import CallIcon from "@mui/icons-material/Call";
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
 const Contact = () => {
   return (
     <Layout>
-        <h1>Contact</h1>
+      <Box sx={{ my: 5, ml: 10, "& h4": { fontWeight: "bold", mb: 2 } }}>
+        <Typography variant="h4">Свяжитесь с нами!</Typography>
+        <p>
+          Оставьте заявку как, где и когда Вам удобно. А наши агенты сделают всю работу за Вас!
+        </p>
+      </Box>
+      <Box
+        sx={{
+          m: 3,
+          width: "600px",
+          ml: 10,
+          "@media (max-width:600px)": {
+            width: "300px",
+          },
+        }}
+      >
+        <TableContainer component={Paper}>
+          <Table aria-label="contact table">
+            <TableHead>
+              <TableRow>
+                <TableCell
+                  sx={{ bgcolor: "black", color: "white" }}
+                  align="center"
+                >
+                  Наши контакты
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <InstagramIcon sx={{ color: "red", pt: 1 }} /> instagram.com/sunrise_warsaw
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <MailIcon sx={{ color: "skyblue", pt: 1 }} /> sunrise.properties.warsaw@gmail.com
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <CallIcon sx={{ color: "green", pt: 1 }} /> +48795793859
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
